0a03d2269f44cd28c69c55db4a96d935d5d54e72
priitmat <priitmat@gmail.com>
2019-10-05 11:33:25 +0300

    First demo
    First demo working

---
+++ FirstApp/FirstApp/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace FirstApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.first_layout);
+
+            var textView = FindViewById<TextView>(Resource.Id.textView1);
+            var button = FindViewById<Button>(Resource.Id.button1);
+
+            button.Click += delegate
+            {
+                textView.Text = "IT WORKS!";
+            };
+        }
+    }
+}
