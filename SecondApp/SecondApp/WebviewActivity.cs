c4d4d5e4b4dcb9bd289db86fac9d517e3fe74486
priitmat <priitmat@gmail.com>
2019-11-02 11:51:47 +0200

    webview koos oma webview kliendiga

--- SecondApp/SecondApp/WebviewActivity.cs
+++ SecondApp/SecondApp/WebviewActivity.cs
@@ -8,6 +8,7 @@ using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
+using Android.Webkit;
 using Android.Widget;
 
 namespace SecondApp
@@ -15,11 +16,26 @@ namespace SecondApp
     [Activity(Label = "WebviewActivity")]
     public class WebviewActivity : Activity
     {
+        WebView _webView;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.webview_layout);
+            _webView = FindViewById<WebView>(Resource.Id.webView1);
+            _webView.Settings.JavaScriptEnabled = true;
+            _webView.SetWebViewClient(new SimpleWebViewClient());
+            _webView.LoadUrl("https://m.delfi.ee");
+        }
 
-            // Create your application here
+        public override bool OnKeyDown([GeneratedEnum] Keycode keyCode, KeyEvent e)
+        {
+            if(keyCode == Keycode.Back && _webView.CanGoBack())
+            {
+                _webView.GoBack();
+                return true;
+            }
+
+            return base.OnKeyDown(keyCode, e);
         }
     }
 }
