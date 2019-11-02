4ffcf93ed0b655851e6443b266d6d0f2421c2afb
priitmat <priitmat@gmail.com>
2019-11-02 09:36:37 +0200

    Lisasin SecondApp projekti

--- FirstApp/FirstApp/MainActivity.cs
+++ FirstApp/FirstApp/MainActivity.cs
@@ -16,11 +16,28 @@ namespace FirstApp
             SetContentView(Resource.Layout.first_layout);
 
             var textView = FindViewById<TextView>(Resource.Id.textView1);
+            var textView2 = FindViewById<TextView>(Resource.Id.textView2);
+
             var button = FindViewById<Button>(Resource.Id.button1);
+            var button2 = FindViewById<Button>(Resource.Id.button2);
+
+            var textfield = FindViewById<EditText>(Resource.Id.editText1);
+            var textfield2 = FindViewById<EditText>(Resource.Id.editText2);
+            
+
+
+
+            var value = textfield.Text;
 
             button.Click += delegate
             {
                 textView.Text = "IT WORKS!";
+            };
+
+            button2.Click += delegate
+            {
+                var test = int.Parse(textfield.Text) + int.Parse(textfield2.Text);
+                textfield2.Text = test.ToString();
             };
         }
     }
