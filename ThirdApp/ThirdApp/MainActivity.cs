c04ab588f9983332860f916d58024dae3b904ffd
priit <priitmat@gmail.com>
2019-11-30 11:04:46 +0200

    esimene listview näide

---
+++ ThirdApp/ThirdApp/MainActivity.cs
@@ -0,0 +1,47 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace ThirdApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            var listView = FindViewById<ListView>(Resource.Id.listView1);
+
+            var countries = new string[] {"Afghanistan","Albania","Algeria","American Samoa","Andorra",
+                    "Angola","Anguilla","Antarctica","Antigua and Barbuda","Argentina",
+                    "Armenia","Aruba","Australia","Austria","Azerbaijan",
+                    "Bahrain","Bangladesh","Barbados","Belarus","Belgium",
+                    "Belize","Benin","Bermuda","Bhutan","Bolivia",
+                    "Bosnia and Herzegovina","Botswana","Bouvet Island","Brazil","British Indian Ocean Territory",
+                    "British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi",
+                    };
+
+            listView.Adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, countries);
+            
+            listView.ItemClick += delegate (object sender, AdapterView.ItemClickEventArgs args)
+            {
+                Toast.MakeText(Application, ((TextView)args.View).Text, ToastLength.Short).Show();
+            };
+
+        }
+
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
